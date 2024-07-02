1100e3d27e7e297c9b00e60d41553b6633c7bc02
chipns <fam.sagi.nadav@gmail.com>
2024-07-02 19:46:40 +0300

    Added the entire project + bug fixes

--- Lakuach/Lakuach/Program.cs
+++ Lakuach/Lakuach/Program.cs
@@ -6,12 +6,15 @@ using System.Threading;
 public class Client
 {
     private static SerialPort _serialPort;
+    private static bool _running = true;
 
     static void Main(string[] args)
     {
         try
         {
-
+            System.Console.WriteLine("Please enter your id: ");
+            string id = System.Console.ReadLine();
+            
             _serialPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
             _serialPort.DataReceived += new SerialDataReceivedEventHandler(Handler);
             _serialPort.Open();
@@ -22,9 +25,10 @@ public class Client
             {
                 System.Console.WriteLine("Please Chose an option (JOIN, NEW, STOP): ");
                 string chosen = System.Console.ReadLine();
-                _serialPort.WriteLine(chosen);
+                _serialPort.WriteLine(chosen+">"+id);
 
             }
+
         }
         catch (Exception ex)
         {
@@ -34,16 +38,16 @@ public class Client
 
     private static void Handler(object sender, SerialDataReceivedEventArgs e)
     {
-        try
-        {
-            string message = _serialPort.ReadLine();
-            Console.WriteLine("Received: " + message);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-    }
+            try
+            {
+                string message = _serialPort.ReadLine();
+                Console.WriteLine("Received: " + message);
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+    }
 }
 
